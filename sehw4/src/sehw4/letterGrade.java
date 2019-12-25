c232179f113f80972e7cd06b326116f18da03a4f
Stanley Zhong <ss19971030@gmail.com>
2019-12-25 08:04:21 +0800

    add main()

--- sehw4/src/sehw4/letterGrade.java
+++ sehw4/src/sehw4/letterGrade.java
@@ -1,7 +1,24 @@
 package sehw4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class letterGrade {
-	
+	public static void main(String[] args) {
+		System.out.print("Enetr the score = ");
+		try {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		int score = Integer.parseInt(br.readLine());
+		 char grade = letterGrade(score);
+		 System.out.println("The grade of " + score + " is " + grade);
+		} catch (NumberFormatException ex) {
+		System.out.println("Not an integer!");
+		} catch (IOException e) {
+		e.printStackTrace();
+		}
+		}
 	public static char letterGrade(int score) {
 		char grade;
 		if (score <0 || score > 100)
